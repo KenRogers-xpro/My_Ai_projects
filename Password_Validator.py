43f047f2ce75c4bf5403585b0d7af62259087c6b
KenRogers-xpro. <kenxpro0@gmail.com>
2025-04-21 23:08:45 +0300

    Added loops to the code

--- Password_Validator.py
+++ Password_Validator.py
@@ -1,54 +1,65 @@
 # this program is supposed to check password validity
 
 def valid_password():
-    user_password = input("Enter your password:")
+    while True:
+        user_password = input("Enter your password:")
 
-    IsEightCharacters = True
-    IsDigit = True
-    IsUppercase = True
-    Count_Char = 0
-    Count_Upper = 0
-    Count_Digit = 0
+        IsEightCharacters = True
+        IsDigit = True
+        IsUppercase = True
+        Count_Char = 0
+        Count_Upper = 0
+        Count_Digit = 0
 
-    for letter in user_password:
-        Count_Char = Count_Char+1
-        if letter.isupper():
-            Count_Upper = Count_Upper + 1
-        
+        for letter in user_password:
+            Count_Char = Count_Char+1
+            if letter.isupper():
+                Count_Upper = Count_Upper + 1
 
-        if letter.isdigit():
-            Count_Digit = Count_Digit + 1
-            # print (Count_Char)
-            # print (letter)
-    
-    if Count_Char >= 8:
-        IsEightCharacters
 
-    else:
-        print ("Password Should Have Eight characters atleast!!!")
-        IsEightCharacters = False
+            if letter.isdigit():
+                Count_Digit = Count_Digit + 1
+                # print (Count_Char)
+                # print (letter)
 
-    if Count_Upper > 0:
-        IsUppercase
+        if Count_Char >= 8:
+            IsEightCharacters
 
-    else :
-        print ("Password Should Have an uppercase character atleast!!!")
-        IsUppercase = False
-        
+        else:
+            print ("Password Should Have Eight characters atleast!!!")
+            IsEightCharacters = False
 
-    if Count_Digit > 0:
-        IsDigit
-    else:
-        print ("Password Should Have one number atleast!!!")
-        IsDigit = False
+        if Count_Upper > 0:
+            IsUppercase
 
-    if IsDigit and IsEightCharacters and IsUppercase :
-        print ("Your Password is strong")
+        else :
+            print ("Password Should Have an uppercase character atleast!!!")
+            IsUppercase = False
 
-    # elif IsDigit and IsEightCharacters and IsUppercase=False :
-    #     print ("Your Password is strong")
-    else:
-        print (" Weak password!!!!!")
+
+        if Count_Digit > 0:
+            IsDigit
+        else:
+            print ("Password Should Have one number atleast!!!")
+            IsDigit = False
+
+        if IsDigit and IsEightCharacters and IsUppercase :
+            print ("Your Password is strong")
+            cont = input("Would you like to test other passwords (y/n):")
+            if cont == "y":
+                valid_password()
+            else:
+                print ("Thanks for using the password validator")
+                break
+
+
+
+        # elif IsDigit and IsEightCharacters and IsUppercase=False :
+        #     print ("Your Password is strong")
+        else:
+            print (" Weak password!!!!!")
+            print (" Try again with a better password!!!!!")
+
 
 
 valid_password()
